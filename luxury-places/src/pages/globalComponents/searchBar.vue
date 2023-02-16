04a8f026b4588d596dfea534e9421c6530eb4263
annacyb <anna.cybulska1@gmail.com>
2023-02-16 13:01:00 +0100

    Home - search section elements created

---
+++ luxury-places/src/pages/globalComponents/searchBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="searchBar">
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+  @use "../../sass/base/reset";
+  @use "../../sass/base/colors";
+  @use "../../sass/base/typo";
+
+    .searchBar {
+        display: flex;
+    }
+
+</style>
+
+
+<script>
+    // import Icon from './Icon.vue'
+
+    export default {
+        // props: ['buttonName', 'iconType', 'iconColor', 'iconSize'],
+        // components: {Icon},
+        data() {
+            return {
+                // iconName: 'iconNavi'
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
--- luxury-places/src/pages/homeComponents/Search.vue
+++ luxury-places/src/pages/homeComponents/Search.vue
@@ -2,8 +2,20 @@
     <div class="background">
         <div class="contentGrid">
             <div class="content">
-                <h1>Section Search</h1> 
-                <p>Lorem ipsum es dolores amet hfie uih eh ui huee huei e eh e efi eief  ei efefh e hee ihu ee i eui</p> 
+                <div class="searchImages">
+                    <img src="../../../images/home/search1.png" alt="Property 1" class="searchImage">
+                    <img src="../../../images/home/search2.png" alt="Property 2" class="searchImage">
+                    <img src="../../../images/home/search3.png" alt="Property 3" class="searchImage">
+                    <img src="../../../images/home/search4.png" alt="Property 4" class="searchImage">
+                    <img src="../../../images/home/search5.png" alt="Property 5" class="searchImage">
+                </div>
+                <div class="sectionTitle">
+                    <hr class="line">
+                    <h2><span class="goldText">200+ exquisite properties </span>to choose from in Switzerland</h2>
+                    <hr class="line">
+                </div>
+                <searchBar></searchBar>
+                <p>Please note that for confidentiality reasons, around 30% of our properties are not listed on this site. Contact our agents for more information.</p> 
             </div>
         </div>
     </div>
@@ -34,13 +46,58 @@
     .content {
         grid-area: content;
     }
+
+    .searchImages {
+        display: flex;
+        place-content: space-evenly;
+        margin-top: 112px;
+    }
+
+    .searchImage {
+        height: auto;
+        width: fit-content;
+        box-shadow: 0px 5px 10px 0px rgba(248, 241, 235, 1);
+    }
+
+    h2 {
+        font-weight: 100;
+    }
+
+    .sectionTitle {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 0px;
+        gap: 16px;
+        margin-top: 16px;
+        margin-bottom: 16px;
+    }
+
+    .goldText {
+        color: colors.$gold1;
+    }
+
+    .line {
+        border-top: 1px solid colors.$grey1;
+        opacity: 0.4;
+        flex-grow: 1;
+    }
+
+    p {
+        text-align: center;
+        font-family: "Segoe UI"!important;
+        font-size: 0.8rem !important;
+        opacity: 0.8;
+        margin-top: 20px;
+    }
     
 </style>
 
 
 <script>
     // import child vue components
-    // import NavBar from './globalComponents/NavBar.vue'
+    import searchBar from "../globalComponents/searchBar.vue"
 
     // firebase settings
     import { collection, getDocs } from "firebase/firestore";
@@ -49,7 +106,7 @@
     import db from "../../firebaseInit";
 
     export default {
-        // components: {NavBar},
+        components: {searchBar},
         data() {
             return {
                 // variables that will be used in HTML
