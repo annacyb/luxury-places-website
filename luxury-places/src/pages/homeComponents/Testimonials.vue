b1ce00ffe55be40017dd4a2f9b3a123bdec4368f
annacyb <anna.cybulska1@gmail.com>
2023-03-09 14:21:55 +0100

    Home - testimonials done

--- luxury-places/src/pages/homeComponents/Testimonials.vue
+++ luxury-places/src/pages/homeComponents/Testimonials.vue
@@ -2,8 +2,63 @@
     <div class="background">
         <div class="contentGrid">
             <div class="content">
-                <div>
-                    <h1>Section Testimonials</h1> 
+                <div class="sectionTitle">
+                    <h2>What Our Clients Say</h2> 
+                    <hr class="line">
+                </div>
+                <div class="testimonials">
+                    <div class="testimonialWrapper">
+                        <div class="testimonialContent">
+                            <div class="stars">
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                            </div>
+                            <div class="testimonialText">
+                                <p>“I highly recommend Luxury Places for anyone looking to purchase a property in Switzerland. They took the time to understand my needs and preferences and used their expertise to find me the perfect property. The entire process was smooth and the end result was exactly what I had been hoping for.”</p>
+                            </div>
+                            <hr class="line2">
+                            <p class="author">David Smith, Founder and CEO of SmithTech Corporation</p>
+                        </div>
+                    </div>
+                    <div class="testimonialWrapper">
+                        <div class="testimonialContent">
+                            <div class="stars">
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                                <div class="star">
+                                    <img src="/icons/starFilled-Testimonials.svg" alt="star">
+                                </div>
+                            </div>
+                            <div class="testimonialText">
+                                <p>“I have high expectations when it comes to service and attention to detail. Luxury Placesy has consistently exceeded my expectations and has provided me with top-notch assistance in finding the perfect property in Swiss Alps.”</p>
+                            </div>
+                            <hr class="line2">
+                            <p class="author">William Mark Thompson, CEO of Thompson Enterprises</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -16,9 +71,30 @@
     @use '../../sass/base/colors';
     @use '../../sass/base/typo';
 
+.testimonials {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+}
+
+.testimonialWrapper {
+    width: 50%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 32px;
+    gap: 32px;
+
+    background: rgba(22, 22, 22, 0.03);
+    border: 1px solid rgba(22, 22, 22, 0.15);
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+}
+
     .background {
         background-color: colors.$white;
-        height: 698px;
+        padding-bottom: 104px;
+        // height: 698px;
         // width: 100vw;
     }
 
@@ -33,6 +109,56 @@
 }
     .content {
         grid-area: content;
+    }
+
+    .sectionTitle {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 0px;
+        gap: 24px; 
+        margin-top: 84px;
+        margin-bottom: 32px;
+    }
+
+    h2 {
+        font-weight: 100;
+    }
+
+    .line {
+        border-top: 1px solid colors.$grey1;
+        opacity: 0.4;
+        flex-grow: 1;
+    }
+    .line2 {
+        border-top: 1px solid colors.$grey1;
+        opacity: 0.25;
+        flex-grow: 1;
+    }
+    .stars {
+        display: flex;
+        gap: 4px;
+        margin-bottom: 4px;
+    }
+
+    p {
+        color: colors.$grey1;
+        font-family: "Segoe UI"!important;
+        font-size: 1rem !important;
+        font-weight: 100;
+        opacity: 0.9;
+        margin-bottom: 16px;
+    }
+
+    p.author {
+        color: colors.$grey1;
+        font-family: "Segoe UI"!important;
+        font-size: 0.8rem !important;
+        font-weight: 100;
+        opacity: 0.9;
+        margin-top: 8px;
+        margin-bottom: 0px;
     }
 
     
